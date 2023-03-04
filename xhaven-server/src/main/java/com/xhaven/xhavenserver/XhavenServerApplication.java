bf4414daa56b52e3192409668fa6c2a9372dada0
AndrzejSasak <andrzej.sasak01@gmail.com>
2023-03-04 19:19:16 +0100

    Initial commit

---
+++ xhaven-server/src/main/java/com/xhaven/xhavenserver/XhavenServerApplication.java
@@ -0,0 +1,13 @@
+package com.xhaven.xhavenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhavenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhavenServerApplication.class, args);
+    }
+
+}
---
+++ xhaven-server/src/test/java/com/xhaven/xhavenserver/XhavenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhaven.xhavenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhavenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
